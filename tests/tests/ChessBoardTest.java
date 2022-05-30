36d62ae010066f43ad0d0225c5fcbdb88fb7d284
M-Moon <34864012+M-Moon@users.noreply.github.com>
2022-05-30 16:34:00 +0100

    Added some basic tests to check out JUnit5. Should use tests to somewhat guide development now

--- tests/tests/ChessBoardTest.java
+++ tests/tests/ChessBoardTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import board.ChessBoard;
+import board.Tile;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,9 +10,28 @@ class ChessBoardTest {
 
     @Test
     void checkFenStringValid() {
+        ChessBoard board = new ChessBoard();
+
+        String fen1 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        assertEquals(true, board.checkFenStringValid(fen1));
+
+        String fen2 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2";
+        assertEquals(true, board.checkFenStringValid(fen2));
+
+        String fen3 = "rnbqkbnr/pp1ppppp/10/2p5/4P3/5N2/PPPP10PPP/RNBQKB1R b KQkq - 1 2"; // added incorrect numbers in the spacings
+        assertEquals(false, board.checkFenStringValid(fen3));
+
+        String fen4 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b Qk - 1 2"; // removed K and q from end of FEN
+        assertEquals(false, board.checkFenStringValid(fen4));
+
+        String fen5 = "rnbqbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQB1R b KQkq - 1 2"; // no kings for either side
+        assertEquals(false, board.checkFenStringValid(fen5));
     }
 
     @Test
     void getBoard() {
+        ChessBoard board = new ChessBoard();
+
+        assertTrue(board.getBoard() != null && board.getBoard() instanceof Tile[]);
     }
 }
